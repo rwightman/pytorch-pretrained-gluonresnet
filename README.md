62e3e8447941e7db12fa1c83a4dec087c9f88efe
Ross Wightman <rwightman@gmail.com>
2019-05-26 11:24:48 -0700

    Add the code, simplified from https://github.com/rwightman/pytorch-image-models

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # pytorch-pretrained-gluonresnet
 
-This will contain a stand-alone version of the pretrained MxNet Gluon ResNet models ported to Pytorch. They are currently part of my model collection (https://github.com/rwightman/pytorch-image-models/blob/master/models/gluon_resnet.py)
+A stand-alone version of the pretrained MxNet Gluon ResNet models ported to Pytorch. Currently part of my model collection (https://github.com/rwightman/pytorch-image-models/blob/master/models/gluon_resnet.py)
 
+This model covers all variants of ResNet, ResNeXt, SE-ResNeXt, and SENet found in the gluon model zoo that
+  * have stride in 3x3 conv layer of bottleneck
+  * have conv-bn-act ordering
+
+Included ResNet variants are:
+  * v1b - 7x7 stem, stem_width=64, same as torchvision ResNet (checkpoint compatible), or NVIDIA ResNet 'v1.5'
+  * v1c - 3 layer deep 3x3 stem, stem_width = 32
+  * v1d - 3 layer deep 3x3 stem, stem_width = 32, average pool in downsample
+  * v1e - 3 layer deep 3x3 stem, stem_width = 64, average pool in downsample  *no pretrained weights available
+  * v1s - 3 layer deep 3x3 stem, stem_width = 64
+
+ResNeXt is standard and checkpoint compatible with torchvision pretrained models. 7x7 stem,
+    stem_width = 64, standard cardinality and base width calcs
+
+SE-ResNeXt is standard. 7x7 stem, stem_width = 64,
+    checkpoints are not compatible with Cadene pretrained, but could be with key mapping
+
+SENet-154 is standard. 3 layer deep 3x3 stem (same as v1c-v1s), stem_width = 64, cardinality=64,
+    reduction by 2 on width of first bottleneck convolution, 3x3 downsample convs after first block
+
+Original ResNet-V1, ResNet-V2 (bn-act-conv), and SE-ResNet (stride in first bottleneck conv) are NOT supported.
---
+++ utils.py
@@ -0,0 +1,73 @@
+import os
+
+
+class AverageMeter:
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+def get_outdir(path, *paths, inc=False):
+    outdir = os.path.join(path, *paths)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    elif inc:
+        count = 1
+        outdir_inc = outdir + '-' + str(count)
+        while os.path.exists(outdir_inc):
+            count = count + 1
+            outdir_inc = outdir + '-' + str(count)
+            assert count < 100
+        outdir = outdir_inc
+        os.makedirs(outdir)
+    return outdir
+
+
+def load_checkpoint(model, checkpoint_path):
+    if checkpoint_path and os.path.isfile(checkpoint_path):
+        print("=> Loading checkpoint '{}'".format(checkpoint_path))
+        checkpoint = torch.load(checkpoint_path)
+        if isinstance(checkpoint, dict) and 'state_dict' in checkpoint:
+            new_state_dict = OrderedDict()
+            for k, v in checkpoint['state_dict'].items():
+                if k.startswith('module'):
+                    name = k[7:]  # remove `module.`
+                else:
+                    name = k
+                new_state_dict[name] = v
+            model.load_state_dict(new_state_dict)
+        else:
+            model.load_state_dict(checkpoint)
+        print("=> Loaded checkpoint '{}'".format(checkpoint_path))
+    else:
+        print("=> Error: No checkpoint found at '{}'".format(checkpoint_path))
+        raise FileNotFoundError()
+
